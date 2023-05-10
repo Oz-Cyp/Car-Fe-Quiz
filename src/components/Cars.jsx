26eccf159c77339769098e4f731df0cb7edf6d64
PembeMistikoglu <pembemistikoglu992@gmail.com>
2023-05-10 11:23:04 +0300

    3 options for fetching data

--- src/components/Cars.jsx
+++ src/components/Cars.jsx
@@ -1,18 +1,70 @@
-import React from "react";
-import { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+// import useApi from "../utils/useAPI";
 import getCars from "../services/cars/getCars";
 
 export const Cars = () => {
-  useEffect(() => {
-    const fetchCars = async () => {
-  const {response} = await getCars();
-  console.log (response)
+  const [cars, setCars] = useState("ola");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  // OPTION 1
+  // -------------------
+  // const [loading, error, response] = useApi(() => getCars());
+  // this is the useApi we use to fetch data. If you dive into it
+  // you will see it uses useState and useEffect.
+  // advantage is that it gives you loading and error state that you can use as below
+
+  // useEffect(() => {
+  //   const { cars } = response;
+  //   setCars(cars);
+  //   console.log(cars);
+  // }, [response]);
+  // -------------------
+
+  // OPTION 2
+  // -------------------
+  const fetchCars = async () => {
+    try {
+      setLoading(true);
+      const response = await getCars();
+      setLoading(false);
+      setCars(response.cars);
+    } catch (error) {
+      setError(true);
+      console.log(error);
     }
+  };
+
+  useEffect(() => {
     fetchCars();
-  }, [])
+  }, []);
+  // -------------------
 
+  // OPTION 3
+  // -------------------
+  // useEffect(() => {
+  //   const fetchCars = async () => {
+  //     try {
+  //       setLoading(true);
+  //       const response = await getCars();
+  //       console.log(response);
+  //       setCars(response.cars);
+  //       setLoading(false);
+  //     } catch (error) {
+  //       setError(true);
+  //       console.log(error);
+  //     }
+  //   };
 
-  return (
-    <div>Hello</div>
-  )
+  //   fetchCars();
+  // }, []);
+
+  if (loading) {
+    return <div>Loading....</div>;
   }
+  if (error) {
+    return <div>Error!!!</div>;
+  }
+
+  return <div>Success</div>;
+};
