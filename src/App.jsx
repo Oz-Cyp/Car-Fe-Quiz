57e7d951d4498ecbcef359a5a34c569bca8c6b90
Ersel Aker <hello@erselaker.com>
2023-09-12 12:06:32 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,36 @@
 import "./App.css";
-import { Searchcar } from "./components/searchcar/searchcar";
-import { Addcar } from "./components/addcar/Addcar";
-import { BrowserRouter, Routes, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { Cars } from "./components/Cars/Cars";
 import { Sales } from "./components/sales/Sales";
 import { Customers } from "./components/customers/Customers";
-import { Inventory } from "./components/Inventory/Inventory";
-// import car from "./img/Car.jpg";
-import { useState } from "react";
-import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  // const [activeTab, setActiveTab] = useState("search");
-
   return (
-    <div className="App">
-      <nav className="Navbar">
-        <BrowserRouter>
-          <Routes>
-            <Route
-              index
-              element={
-                <div className="Navbar">
-                  <Link className="link" to="/Cars">
-                    Cars
-                  </Link>
-                  <Link className="link" to="/Sales">
-                    {" "}
-                    Sales{" "}
-                  </Link>
-                  <Link className="link" to="/Customers">
-                    Customers
-                  </Link>
-                </div>
-              }
-            />
-            <Route path="/Cars" element={<Cars />} />
-            <Route path="Search" element={Searchcar}></Route>
-            <Route path="Search" element={Addcar}></Route>
-            <Route path="Search" element={Inventory}></Route>
-            <Route path="/Sales" element={<Sales />} />
-            <Route path="/Customers" element={<Customers />} />
-
-            <Route path="*" element={<div>404 Not Found</div>} />
-          </Routes>
-        </BrowserRouter>
-      </nav>
-      
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <nav className="Navbar">
+          <div className="Navbar">
+            <Link className="link" to="/Cars">
+              Cars
+            </Link>
+            <Link className="link" to="/Sales">
+              {" "}
+              Sales{" "}
+            </Link>
+            <Link className="link" to="/Customers">
+              Customers
+            </Link>
+          </div>
+        </nav>
+        <Routes>
+          <Route index element={<Cars />} />
+          <Route path="/Cars" element={<Cars />} />
+          <Route path="/Sales" element={<Sales />} />
+          <Route path="/Customers" element={<Customers />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
